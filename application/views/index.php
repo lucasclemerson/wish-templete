2f7addb09a7e5183cff59634d5c18dce1a828822
clemerson <clemerson.lucas.oliveira@gmail.com>
2023-01-09 17:31:01 -0300

    consegui o modelo padrao ja determinado

---
+++ application/views/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Login</title>
+<link rel="stylesheet" href="../../uploads/css/main.css">
+</head>
+<body>
+<div class="page">
+<form method="POST" class="formLogin">
+<h1>Bem vindo ao nosso sistema!</h1>
+<p>Digite os seus dados de acesso no campo abaixo para mantermos contato.</p>
+<label for="nome">Nome completo</label>
+<input type="text" placeholder="Digite seu nome" autofocus="true" />
+<label for="email">E-mail</label>
+<input type="email" placeholder="Digite seu e-mail" autofocus="true" />
+<a target="_blank" href="https://inexts.atlantic.tec.br/account/trabalheconosco">Queria fazer parte do desenvolvimento</a>
+<input type="submit" value="Acessar" class="btn" />
+</form>
+</div>
+
+</body>
+</html>
