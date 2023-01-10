c2a9dde845dde0de8adcb261cc24775822f72de1
clemerson <clemerson.lucas.oliveira@gmail.com>
2023-01-09 21:29:56 -0300

    criação de um arquivo para o cabeçario

--- application/views/index.php
+++ application/views/index.php
@@ -4,65 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Wish - Comprar ficou mais divertido!</title>
     <link rel="shortcut icon" href="../../uploads/imagens/favicon.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <link rel="stylesheet" href="../../uploads/css/main.css">
-    
-    
-
 </head>
-<body class="container-fluid">
-    <header> 
-        <div class="row justify-content-md-center">
-            <nav class="navbar">
-                <div class="col-xs-6">
-                    <img class="mb-2" height="30" width="100" src="../../uploads/imagens/logo-horizontal.png" alt="Imagem da logo">
-                </div>
-                
-                <div class="col-sx-6">
-                    <form class="d-flex" role="search">
-                        <div class="input-group mb-3">
-                            <input style="width:300px;" type="search" class="form-control pt-2 pb-2" placeholder="O que você deseja encontrar?" aria-label="search" aria-describedby="button-addon2">
-                            <button class="btn btn-info text-white" id="button-addon2"  type="submit">Pesquisar</button>
-                        </div>
-                        <a href="#" class="text-info texto-destacado">
-                            Sign In
-                        </a>
-
-                        <a href="" class="texto-destacado-icone text-dark">
-                           <i class="bi bi-bell-fill"></i> 
-                        </a>
-
-                        <a href="" class="texto-destacado-icone text-dark">
-                            <i class="bi bi-cart-fill"></i>
-                        </a>
-
-                        <a href="" class="texto-destacado-icone text-dark">
-                            <i class="bi bi-heart-fill"></i>
-                        </a>
-                    </form>
-                </div>
-            </nav>
-            <div class="row">
-                <ul class="nav justify-content-center">
-                    <li class="nav-item">
-                        <a class="texto-escuro texto-lista" aria-current="page" href="#">Popular</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="texto-escuro texto-lista" aria-current="page" href="#">Express</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="texto-escuro texto-lista" aria-current="page" href="#">Visualizado recentemente</a>
-                    </li>
-
-
-                </ul>
-            </div>
-        </div>
-
-    </header>
-
+<body class="container m-0 p-0">
+    <?php require "header.php"; ?>
 </body>
 </html>
